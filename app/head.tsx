8da3ddae319d3850d1b8227507a2aaae20d1138d
Mukarram Ali <mukarramaliofficial@gmail.com>
2023-01-26 00:26:56 +0500

    Added hero skills

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>MUKARRAM ALI PORTFOLIO</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <meta name="Mukarram Ali" content="email4mukarram" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="https://images.ctfassets.net/bpbqpx524mlk/1YwimNfxaQdRFHKIphRVPQ/72eaf6b3c88027244863ef12375fc36d/WhatsApp_Image_2022-12-16_at_11.23.27_PM.jpeg?h=250" />
     </>
   )
 }
