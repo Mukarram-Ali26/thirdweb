fe372fd13d3c91ac2b3bc63017170b40f743431c
Mukarram Ali <mukarramaliofficial@gmail.com>
2023-04-27 01:14:48 +0500

    updated function in dynamic route

--- app/news/[...id]/page.tsx
+++ app/news/[...id]/page.tsx
@@ -1,19 +1,19 @@
 import Post from '@/app/components/Post';
 import React, { Suspense } from 'react'
+async function getNews(id:string) {
+    const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_ID}/entries/${id}?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`, { cache: 'no-store' });
 
-const page = async ({ params }: { params: { id: string } }) => {
-    async function getNews() {
-        const res = await fetch(`https://cdn.contentful.com/spaces/${process.env.NEXT_PUBLIC_CONTENTFUL_ID}/entries/${params?.id}?access_token=${process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN}`, { cache: 'no-store' });
-
-        // Recommendation: handle errors
-        if (!res.ok) {
-            // This will activate the closest `error.js` Error Boundary
-            throw new Error('Failed to fetch data');
-        }
-
-        return res.json();
+    // Recommendation: handle errors
+    if (!res.ok) {
+        // This will activate the closest `error.js` Error Boundary
+        throw new Error('Failed to fetch data');
     }
-    const news = await getNews();
+
+    return res.json();
+}
+const page = async ({ params }: { params: { id: string } }) => {
+   
+    const news = await getNews(params.id);
     // console.log(news);
     // console.log(params?.id);
 
